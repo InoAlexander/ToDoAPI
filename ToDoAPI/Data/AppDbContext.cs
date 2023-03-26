73383de70c7dacfa5a9c9e9dc180e7d63686934e
Ino_A <inoalexander007@gmail.com>
2023-03-26 01:16:00 -0700

    adding db configs for db context

---
+++ ToDoAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+        // db set is a representation of the TODO items in our database
+        public DbSet<ToDo> ToDos => Set<ToDo>();
+    }
+}
