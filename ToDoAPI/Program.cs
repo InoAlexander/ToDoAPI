e01687a6aaa0b1dd3a90278bfea7d8200e4a2da3
Ino_A <inoalexander007@gmail.com>
2023-03-26 01:20:52 -0700

    adding the connection string to program.cs

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 // Add services to the container.
 
