952c7989be3d0f53285d6576ef88a224dfd4a15a
Ino_A <inoalexander007@gmail.com>
2023-03-26 01:34:19 -0700

    trying to add migrations its not working

--- ToDoAPI/Models/ToDo.cs
+++ ToDoAPI/Models/ToDo.cs
@@ -1,6 +1,12 @@
-﻿namespace ToDoAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models
 {
     public class ToDo
     {
+        [Key]
+        public int Id { get; set; }
+        public string? ToDoName { get; set; }
+        
     }
 }
