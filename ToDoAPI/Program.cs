f69c73d61d5002b6859c7ae379a8413092c8af12
Ino_A <inoalexander007@gmail.com>
2023-03-26 20:55:00 -0700

    adding the update put method

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -19,13 +19,30 @@ app.MapGet("api/todo", async (AppDbContext context) =>
     var items = await context.ToDos.ToListAsync();
     return Results.Ok(items);
 });
-
+// post end point
 app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
 {
     await context.ToDos.AddAsync(toDo);
     await context.SaveChangesAsync();
     // passing back the location where the resource can be found
     return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
+// update needs to have the id, will also expect an integer id
+app.MapPut("api/todo/{id}", async (AppDbContext context, int id, ToDo toDo) =>
+{
+    // checking that the resource we are updating exists
+    var toDoModel = await context.ToDos.FirstOrDefaultAsync(t => t.Id == id);
+    // assuming its not null there will be something to update
+    if (toDoModel == null) 
+    {
+        return Results.NotFound();
+    }     
+    // will update manually
+    toDoModel.ToDoName = toDo.ToDoName;
+    // saving the update changes
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
 });
 
 app.Run();
